2a316b63d5e55e5fb006d6182b864e110eab5c73
artemka1511 <artemka15111992@gmail.com>
2023-02-22 23:51:00 +0700

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using Calculator.Interfaces;
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new Logger();
+            ISummation calculator = new Calculator(logger);
+            calculator.Sum();
+        }
+
+        public class Logger : ILogger
+        {
+            void ILogger.Error(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+
+            void ILogger.Event(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(message);
+                Console.ResetColor();
+            }
+        }
+        public class Calculator : ISummation
+        {
+            ILogger Logger { get; set; }
+
+            public Calculator(ILogger logger)
+            {
+                Logger = logger;
+            }
+            void ISummation.Sum()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Logger.Event("Введите первое число: ");
+                        var firstNum = Convert.ToInt32(Console.ReadLine());
+                        Logger.Event("Введите второе число: ");
+                        var secondNum = Convert.ToInt32(Console.ReadLine());
+                        Logger.Event($"Результат суммирования равен {firstNum + secondNum}");
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Logger.Error("Ошибка! Введено некорректное значение. Повторите ввод значений");
+                    }
+                }
+            }
+        }
+    }
+}
